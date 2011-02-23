c9a4b1c2d5941ca49af05881637613787e133adf
Lasse Bunk <lassebunk@gmail.com>
2011-02-23 21:53:15 +0100

    first commit

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,31 @@
+class PhotosController < ApplicationController
+  def create
+    @photo = Photo.new(params[:photo])
+    @photo.image = File.new(upload_path)
+    @photo.save
+
+    redirect_to @photo
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+  def index
+    @photos = Photo.all
+  end
+
+  def upload
+    File.open(upload_path, 'w') do |f|
+      f.write request.raw_post
+    end
+    render :text => "ok"
+  end
+
+  private
+
+  def upload_path # is used in upload and create
+    file_name = session[:session_id].to_s + '.jpg'
+    File.join(RAILS_ROOT, 'public', 'uploads', file_name)
+  end
+end
